b94e68a8cb4c3bcc20c3cfc7efba5e62373353d8
Oliver Goldstein <oliverjgoldstein@gmail.com>
2019-06-07 17:31:07 +0100

    Doubler and reverser modification of handlers

--- mover.sh
+++ mover.sh
@@ -1,3 +1,6 @@
+# export KOKAFILE=/Users/Development/Documents/Dissertation/koka/test/ojg/test.kk
+# export KOKADIR=/Users/Development/Documents/Dissertation/koka
+
 source ~/.bash_profile
 cp test.kk $KOKAFILE
 cd $KOKADIR
