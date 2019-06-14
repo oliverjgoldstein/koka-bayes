f1622e2c44f1d4878513e623c200992752d16bfc
Oliver Goldstein <oliverjgoldstein@gmail.com>
2019-06-14 14:22:04 +0100

    Split everything into different files

--- mover.sh
+++ mover.sh
@@ -2,6 +2,8 @@
 # export KOKADIR=/Users/Development/Documents/Dissertation/koka
 
 source ~/.bash_profile
-cp test.kk $KOKAFILE
+cp koka-bayes.kk $KOKAFILE
+cp exp.kk $KOKAFILE
+cp bayes-handlers.kk $KOKAFILE
 cd $KOKADIR
 jake
