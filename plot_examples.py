b8b9c5d6086e57046238b712fd85c4d16547a024
Oliver Goldstein <oliverjgoldstein@gmail.com>
2019-06-27 19:05:47 +0100

    First working version

--- plot_examples.py
+++ plot_examples.py
@@ -1,13 +1,23 @@
-
-import matplotlib.pyplot as plt
+import random
+import numpy
+from matplotlib import pyplot
 import csv
 
 f =  './output.csv'
 data = csv.reader(open(f, 'rt'))
 li = [tuple(row) for row in data]
+li = li[:len(li)-1]
+x = [float(v[0]) for v in li]
+y = [float(v[1]) for v in li]
+print(sum(x))
 
-x = [v[0] for v in li]
-y = [v[1] for v in li]
+pyplot.plot(y,x, 'ro')
+pyplot.savefig('posterior.png')
+pyplot.figure()
 
-plt.plot(y,x, 'ro')
-plt.savefig('posterior.png')
+bins = numpy.linspace(0.9, 1.1, 20)
+
+# pyplot.hist(x, bins, alpha=0.5, label='x')
+pyplot.hist(y, alpha=0.5, label='y')
+pyplot.legend(loc='upper right')
+pyplot.savefig('posterior_hist.png')
