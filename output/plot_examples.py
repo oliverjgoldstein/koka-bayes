f9060825e5e323ea0d9e6024cc09284cdc576d76
Nothing <10864811+theneuroticnothing@users.noreply.github.com>
2019-07-10 19:37:17 +0100

    Continuing on the model

--- output/plot_examples.py
+++ output/plot_examples.py
@@ -3,25 +3,61 @@ import numpy
 from matplotlib import pyplot
 import csv
 
-f =  ['temperature_posterior', 'gaussian_posterior']
+# f =  ['gaussian_posterior']
+# for i in range(0, len(f)):
+#     data = csv.reader(open(f[i] + '.csv', 'rt'))
+#     li = [tuple(row) for row in data]
+#     li = li[:len(li)-1]
+#     x = [float(v[0]) for v in li]
+#     y = [float(v[1]) for v in li]
+# 
+#     pyplot.figure()
+# 
+#     pyplot.plot(y,x, 'ro')
+#     pyplot.ylabel('likelihood')
+#     pyplot.xlabel('theta')
+#     pyplot.savefig(f[i] + '_points.png')
+# 
+#     pyplot.figure()
+# 
+#     pyplot.hist(y, alpha=0.5, label='y')
+#     pyplot.xlabel('theta')
+#     pyplot.ylabel('count')
+#     pyplot.legend(loc='upper right')
+#     pyplot.savefig(f[i] + '_hist.png')
+
+
+f = ['temperature_posterior']
 for i in range(0, len(f)):
     data = csv.reader(open(f[i] + '.csv', 'rt'))
     li = [tuple(row) for row in data]
     li = li[:len(li)-1]
-    x = [float(v[0]) for v in li]
-    y = [float(v[1]) for v in li]
+    x1 = [float(v[0]) for v in li]
+    y1 = [float(v[1]) for v in li]
 
     pyplot.figure()
 
-    pyplot.plot(y,x, 'ro')
+    pyplot.plot(y1,x1, 'ro')
     pyplot.ylabel('likelihood')
     pyplot.xlabel('theta')
     pyplot.savefig(f[i] + '_points.png')
 
     pyplot.figure()
 
-    pyplot.hist(y, alpha=0.5, label='y')
+    pyplot.hist(y1, alpha=0.5, label='y')
     pyplot.xlabel('theta')
     pyplot.ylabel('count')
     pyplot.legend(loc='upper right')
-    pyplot.savefig(f[i] + '_hist.png')
+    pyplot.savefig(f[i] + '_hist_1.png')
+    
+    y2 = [float(v[2]) for v in li]
+    
+    pyplot.figure()
+    
+    pyplot.hist(y2, alpha=0.5, label='y')
+    pyplot.xlabel('theta')
+    pyplot.ylabel('count')
+    pyplot.legend(loc='upper right')
+    pyplot.savefig(f[i] + '_hist_2.png')
+    
+    
