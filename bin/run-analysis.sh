7afd68c8236e4fa8f96054b6a58670af913ef4f0
Nothing <10864811+theneuroticnothing@users.noreply.github.com>
2019-07-14 15:26:16 +0100

    open output.html on analyze

--- bin/run-analysis.sh
+++ bin/run-analysis.sh
@@ -1,2 +1,9 @@
 cd ./output/
 python ./plot_examples.py
+printf "Output written to: %s/output.html\n" "$(pwd)"
+
+case "$OSTYPE" in
+  darwin*)  open     output.html ;; 
+  *)        xdg-open output.html ;;
+esac
+
