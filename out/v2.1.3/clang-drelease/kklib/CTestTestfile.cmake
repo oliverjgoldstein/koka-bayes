bf070292564279271d7fe551936249ad732aabb7
Oliver J Goldstein <oliver.goldstein@kellogg.ox.ac.uk>
2023-03-27 19:26:15 +0100

    Working with Koka 2

--- out/v2.1.3/clang-drelease/kklib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/local/share/koka/v2.1.3/kklib
-# Build directory: /Users/Oliver/Documents/Computer Science/Year 7/Koka/eff-bayes/code/out/v2.1.3/clang-drelease/kklib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
